fd53af88dcf7ad9d2cdc9acadff808a5aa64b439
fongchun <fongchunchan@gmail.com>
2015-07-15 18:15:54 +0000

    Adding GenomicRanges Test Data

---
+++ R/data-GRCh37.72.gr.R
@@ -0,0 +1,5 @@
+#' GenomicRanges of Chromosome 1 in GRCh37.72
+#'
+#' A GenomicRanges object containing 2000 bp windows downstream of 
+#' the transcription start sites of all genes on chromsome 1
+"gr"
